fb4fb81052240f9d6c975ed1afa5dc5928c3384d
Asim-Shahriar <asimshariar@gmail.com>
2020-05-27 01:46:56 +0600

    Note model class,Notedatabase class,dao class created

---
+++ app/src/main/java/com/example/mvvmpractise/NoteDao.java
@@ -0,0 +1,29 @@
+package com.example.mvvmpractise;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+import static android.icu.text.MessagePattern.ArgType.SELECT;
+
+public interface NoteDao {
+
+    @Insert
+    void insert(Note note);
+
+    @Update
+    void update(Note note);
+
+    @Delete
+    void delete(Note note);
+
+    @Query("DELETE FROM note_table")
+    void deleteAllNotes();
+
+    @Query("SELECT * FROM note_table ORDER BY priority DESC")
+    LiveData<List<Note>> getAllNotes();
+}
