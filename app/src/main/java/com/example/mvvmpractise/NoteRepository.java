49f82f314416cf09f37c2dde5895d5e7f97bf627
Asim-Shahriar <asimshariar@gmail.com>
2020-06-02 21:56:51 +0600

    Note repository class completed

--- app/src/main/java/com/example/mvvmpractise/NoteRepository.java
+++ app/src/main/java/com/example/mvvmpractise/NoteRepository.java
@@ -23,15 +23,17 @@ public class NoteRepository {
    new InsertAsyncTask(noteDao).execute(note);
   }
   public void update(Note note){
-
+   new UpdateAsyncTask(noteDao).execute(note);
   }
   public void delete(Note note){
-
+   new DeleteAsyncTask(noteDao).execute(note);
   }
   public void deleteAllNotes(){
-
+   new DeleteAllAsyncTask(noteDao).execute();
   }
+
   public LiveData<List<Note>> getAllNotes(){
+
       return allNotes;
   }
 
@@ -46,6 +48,47 @@ public class NoteRepository {
       protected Void doInBackground(Note... notes) {
          noteDao.insert(notes[0]);
          return null;
+      }
+  }
+
+  private static class UpdateAsyncTask extends AsyncTask<Note,Void,Void>{
+
+      private NoteDao noteDao;
+      private UpdateAsyncTask(NoteDao noteDao){
+          this.noteDao=noteDao;
+      }
+
+      @Override
+      protected Void doInBackground(Note... notes) {
+          noteDao.update(notes[0]);
+          return null;
+      }
+  }
+
+  private static class DeleteAsyncTask extends AsyncTask<Note,Void,Void>{
+
+      private NoteDao noteDao;
+      private DeleteAsyncTask(NoteDao noteDao){
+          this.noteDao=noteDao;
+      }
+      @Override
+      protected Void doInBackground(Note... notes) {
+          noteDao.delete(notes[0]);
+          return null;
+      }
+  }
+
+  private static class DeleteAllAsyncTask extends AsyncTask<Note,Void,Void>{
+
+      private NoteDao noteDao;
+      private DeleteAllAsyncTask(NoteDao noteDao){
+          this.noteDao=noteDao;
+      }
+
+      @Override
+      protected Void doInBackground(Note... notes) {
+          noteDao.deleteAllNotes();
+          return null;
       }
   }
 }
