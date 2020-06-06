099f401d2a4188551355ae9410047956b9bfc834
Asim-Shahriar <asimshariar@gmail.com>
2020-06-06 21:31:47 +0600

    onActivity result code added

--- app/src/main/java/com/example/mvvmpractise/MainActivity.java
+++ app/src/main/java/com/example/mvvmpractise/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mvvmpractise;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
@@ -8,6 +9,7 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -16,6 +18,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private NoteViewModel noteViewModel;
+    public static final int ADD_NOTE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,5 +39,23 @@ public class MainActivity extends AppCompatActivity {
                 noteAdapter.setNotes(notes);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==ADD_NOTE_REQUEST && resultCode==RESULT_OK){
+
+            String title=data.getStringExtra(AddNoteActivity.EXTRA_TITLE);
+            String description=data.getStringExtra(AddNoteActivity.EXTRA_DESCRIPTION);
+            int priority=data.getIntExtra(AddNoteActivity.EXTRA_PRIORITY,1);
+
+            Note note=new Note(title,description,priority);
+            noteViewModel.insert(note);
+            Toast.makeText(this, "Note saved", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Note not saved", Toast.LENGTH_SHORT).show();
+        }
     }
 }
